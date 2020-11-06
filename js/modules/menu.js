f10fc0d4ebb3b6c6a246c1089cf3240cd2ff538e
vargattj <vargattjh@gmail.com>
2020-11-06 09:51:01 -0300

    slide background index

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -1,21 +1,23 @@
 export default function iniciarMenu() {
-  let show = false;
+  let show = true;
 
-  const categorias = document.querySelector(".categorias");
-  const menuToggle = document.querySelector(".menu-burguer");
+  const sideMenu = document.querySelector(".side-menu");
+  const menuToggle = document.querySelector(".menu");
 
-  menuToggle.addEventListener("click", () => {
-    document.body.style.overflow = show ? "hidden" : "initial";
-    categorias.classList.toggle("on", show);
+  menuToggle.addEventListener("click", (e) => {
+    // document.body.style.overflow = show ? "hidden" : "initial";
+    e.preventDefault();
+    console.log('a');
+    sideMenu.classList.toggle("on", show);
     show = !show;
   });
 
-  categorias.addEventListener("click", (e) => {
-    if (e.target === categorias) {
-      categorias.classList.toggle("on", show);
-    }
-    document.body.style.overflow = show ? "hidden" : "initial";
+  // categorias.addEventListener("click", (e) => {
+  //   if (e.target === categorias) {
+  //     sideMenu.classList.toggle("on", show);
+  //   }
+  //   document.body.style.overflow = show ? "hidden" : "initial";
 
-    show = !show;
-  });
+  //   show = !show;
+  // });
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import iniciarDropdown from './modules/dropdown.js';
 import iniciarMenu from './modules/menu.js';
+import multiBanner from './modules/multiBanner.js';
 
 iniciarDropdown();
 iniciarMenu();
+multiBanner();
