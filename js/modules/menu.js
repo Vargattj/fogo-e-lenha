0093572b9c0eded5127ede0453e4f4f3c0a7c691
vargattj <vargattjh@gmail.com>
2020-11-09 13:38:51 -0300

    final

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -5,19 +5,8 @@ export default function iniciarMenu() {
   const menuToggle = document.querySelector(".menu");
 
   menuToggle.addEventListener("click", (e) => {
-    // document.body.style.overflow = show ? "hidden" : "initial";
     e.preventDefault();
-    console.log('a');
     sideMenu.classList.toggle("on", show);
     show = !show;
   });
-
-  // categorias.addEventListener("click", (e) => {
-  //   if (e.target === categorias) {
-  //     sideMenu.classList.toggle("on", show);
-  //   }
-  //   document.body.style.overflow = show ? "hidden" : "initial";
-
-  //   show = !show;
-  // });
 }
