6227e1c87fb67bb99f3e7651e3b9a2860acf8f26
vargattj <vargattjh@gmail.com>
2020-11-09 14:07:05 -0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
-// import iniciarDropdown from "./modules/dropdown.js";
 import iniciarMenu from "./modules/menu.js";
 import multiBanner from "./modules/multiBanner.js";
 import scrollNavigation from "./modules/scrollNavigation.js";
 
-// export function pageMenu() {
-//   iniciarMenu();
-//   slider();
-// }
 iniciarMenu();
 multiBanner();
 scrollNavigation();
